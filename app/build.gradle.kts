a8d30f9d2107ad7f9b2f844bf2d902f886862e1f
Dmitry Kryushnikov <kryushnikov.d@gmail.com>
2026-09-14 12:32:34 +0500

    fix version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,6 +82,9 @@ android {
         }
     }
     buildTypes {
+        debug {
+            applicationIdSuffix = ".debug"
+        }
         release {
             isMinifyEnabled = true
             isShrinkResources = true
